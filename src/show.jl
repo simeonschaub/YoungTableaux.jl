cc7621bcc4c14e538c5d7930a8682769c3084713
Simeon David Schaub <schaub@mit.edu>
2023-10-16 18:59:05 +0200

    fix demo

--- src/show.jl
+++ src/show.jl
@@ -1,6 +1,7 @@
 using UUIDs
 
-function Base.show(io::IO, ::MIME"text/html", (; rows)::YoungTableau)
+function Base.show(io::IO, ::MIME"text/html", yt::YoungTableau)
+    (; rows) = yt
     class = string("yt", uuid1())
     print(io, """
         <table style='border: none'>
@@ -8,19 +9,22 @@ function Base.show(io::IO, ::MIME"text/html", (; rows)::YoungTableau)
             .$class {
                 border: 1pt solid;
                 width: $(100 / (isempty(rows) ? 100 : length(rows[1])))%;
-                padding: 4px;
+                padding: 8px;
             }
             .$class div {
-                aspect-ratio: 1;
-                text-align: center;
                 font-size: large;
+                aspect-ratio: 1;
+                min-width: max($(maximum(textwidth∘string, yt))ch, 1em);
+                display: flex;
+                align-items: center;
+                justify-content: center;
             }
         </style>
         """)
     for row in rows
         print(io, "<tr>")
         for m in row
-            print(io, "<td class=$class><div>", m, "</div></td>")
+            print(io, "<td class=$class><div><span>", m, "</span></div></td>")
         end
         print(io, "</tr>")
     end

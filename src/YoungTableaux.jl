9898437f1b0170e15706e7a1fe853e3a86a2f232
Simeon David Schaub <schaub@mit.edu>
2023-10-16 15:57:14 +0200

    initial interface

--- src/YoungTableaux.jl
+++ src/YoungTableaux.jl
@@ -1,5 +1,29 @@
 module YoungTableaux
 
-# Write your package code here.
+export YoungTableau, rows
+
+abstract type AbstractYoungTableau{T} end
+
+struct YoungTableau <: AbstractYoungTableau{Int}
+    rows::Vector{Vector{Int}}
+end
+YoungTableau() = YoungTableau(Vector{Int}[])
+
+rows((; rows)::YoungTableau) = rows
+
+function Base.size(yt::AbstractYoungTableau)
+    r = rows(yt)
+    return length(r), isempty(r) ? 0 : length(first(r))
+end
+function Base.getindex(yt::AbstractYoungTableau{T}, i::Int, j::Int) where {T}
+    r = rows(yt)
+    isassigned(r, i) || throw(BoundsError(yt, (i, j)))
+    isassigned(first(r), j) || throw(BoundsError(yt, (i, j)))
+    row = r[i]
+    return get(row, j, zero(T))
+end
+Base.IteratorSize(::AbstractYoungTableau) = Base.SizeUnknown()
+Base.eltype(::AbstractYoungTableau{T}) where {T} = T
+Base.iterate(yt::AbstractYoungTableau, st...) = iterate(Iterators.flatten(rows(yt)), st...)
 
 end

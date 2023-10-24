7411899bd15e5033eb65a8298c0b2d34f34c0b3d
Simeon Schaub <simeondavidschaub99@gmail.com>
2023-10-24 23:01:52 +0200

    Animate (#1)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * export Pluto notebooks

--- src/show.jl
+++ src/show.jl
@@ -5,6 +5,7 @@ _string_entry(yt) = m -> sprint(_show_entry, yt, m)
 function Base.show(io::IO, ::MIME"text/html", yt::AbstractYoungTableau)
     rows = YoungTableaux.rows(yt)
     class = get(io, :html_class, string("yt-", uuid1()))
+    maxlen = get(io, :html_maxlen, maximum(textwidth∘_string_entry(yt), yt; init=0))
     print(io, """
         <table style='border: none'>
         <style scoped>
@@ -16,7 +17,7 @@ function Base.show(io::IO, ::MIME"text/html", yt::AbstractYoungTableau)
             .$class div {
                 font-size: large;
                 aspect-ratio: 1;
-                min-width: max($(maximum(textwidth∘_string_entry(yt), yt; init=0))ch, 1em);
+                min-width: max($(maxlen)ch, 1em);
                 display: flex;
                 align-items: center;
                 justify-content: center;
@@ -33,4 +34,84 @@ function Base.show(io::IO, ::MIME"text/html", yt::AbstractYoungTableau)
         print(io, "</tr>")
     end
     print(io, "</table>")
+end
+
+using HypertextLiteral
+
+function visualize_insert(yt, x, x_row; to_replace=nothing, delete=false)
+    class = string("vis-container-", uuid1())
+    class_yt = string("yt-", uuid1())
+    maxlen = max(maximum(textwidth∘_string_entry(yt), yt; init=0), textwidth(_string_entry(yt)(x)))+1
+    template = join((
+        "\"$(join(
+                  [fill(i > nrows(yt) ? "." : "yt", rowlength(yt)); "."; i == x_row ? "x" : ". "],
+                  " ",
+                 ))\"" for i in 1:nrows(yt)+1),
+        "\n",
+    )
+    @htl """
+    <style>
+    @keyframes replace {
+      from {}
+      to {transform: translateX($(-100*(rowlength(yt) - to_replace[2] + 2))%);}
+    }
+    @keyframes pop {
+      0% {}
+      50% {transform: translateY(-100%);}
+      100% {
+        transform: translateY(100%) translateX($(100*(rowlength(yt) - to_replace[2] + 3))%);
+        outline: 1pt solid;
+      }
+    }
+
+    .$class {
+    	display: grid;
+    	/*grid-template-columns: $(rowlength(yt))fr repeat(2, 1fr);*/
+        grid-auto-columns: 1fr;
+    	width: fit-content;
+    	margin: auto;
+    	margin-block-end: var(--pluto-cell-spacing);
+    	margin-block-start: var(--pluto-cell-spacing);
+        grid-template: $(template);
+    }
+
+    .$class #yt {
+    	grid-area: yt;
+    }
+    .$class #yt table {
+    	margin: 0;
+    }
+    .$class #x {
+        grid-area: x;
+        margin-left: -1pt;
+        margin-right: 1pt;
+        background-color: green;
+        $(if delete
+            @htl """
+            animation-name: replace;
+            animation-duration: 1s;
+            animation-fill-mode: forwards;
+            """
+        end)
+    }
+    $(if to_replace !== nothing
+        @htl """
+        .$class #c$(to_replace[1])-$(to_replace[2]) {
+            background-color: darkred;
+            $(if delete
+                @htl """
+                animation-name: pop;
+                animation-duration: 2s;
+                animation-fill-mode: forwards;
+                """
+            end)
+        }
+        """
+    end)
+    </style>
+    <div class=$class>
+    <div id=yt>$(HTML(repr("text/html", yt; context=IOContext(devnull, :html_class => class_yt, :html_maxlen => maxlen))))</div>
+    <div id=x class=$class_yt style="font-family: var(--julia-mono-font-stack); width: auto"><div><span>$(_string_entry(yt)(x))</span></div></div>
+    </div>
+    """
 end

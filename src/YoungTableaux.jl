e32d4ac8a819ec582a3d9ef4dfc1109a7e7c394f
Simeon David Schaub <simeon@schaub.rocks>
2023-11-26 16:07:01 +0100

    add some docstrings

--- src/YoungTableaux.jl
+++ src/YoungTableaux.jl
@@ -6,13 +6,36 @@ export ≺, ≻, ≺′, ≻′
 
 using MappedArrays
 
+"""
+    abstract type AbstractDiagram{T} end
+
+Supertype for any kind of object that has the shape of a Young diagram, filled or not
+"""
 abstract type AbstractDiagram{T} end
+"""
+    abstract type AbstractYoungTableau{T} <: AbstractDiagram{T} end
+
+Supertype for any fully filled Young diagram
+"""
 abstract type AbstractYoungTableau{T} <: AbstractDiagram{T} end
 
+"""
+    YoungTableau{T}(rows::Vector{Vector{T}})
+
+Vector of rows describing a (filled) Young tableau
+"""
 struct YoungTableau{T} <: AbstractYoungTableau{T}
     rows::Vector{Vector{T}}
 end
 YoungTableau{T}() where {T} = YoungTableau(Vector{T}[])
+
+"""
+    rows(::AbstractDiagram)
+
+Returns an array containing all rows of a diagram. Must be implemented by any subtype of
+`AbstractDiagram`. Unfilled diagrams should return a `Bool`
+"""
+function rows end
 
 rows((; rows)::YoungTableau) = rows
 function row(yt, i::Int)
@@ -60,9 +83,24 @@ end
 
 Base.copy((; rows)::YoungTableau) = YoungTableau(copy.(rows))
 
+"""
+    abstract type AbstractShape <: AbstractDiagram{Bool} end
+
+A diagram only describing a certain shape, potentially with holes such as a skew partition
+"""
 abstract type AbstractShape <: AbstractDiagram{Bool} end
+"""
+    abstract type AbstractPartition <: AbstractShape end
+
+Description of only the outer shape of a diagram
+"""
 abstract type AbstractPartition <: AbstractShape end
 
+"""
+    Partition(parts::Vector{Int})
+
+A partition with row lengths given by `parts`
+"""
 struct Partition <: AbstractPartition
     parts::Vector{Int}
 end
@@ -83,8 +121,13 @@ ncols((; diagram)::PartitionOf, i::Int) = ncols(diagram, i)
 nrows((; diagram)::PartitionOf) = nrows(diagram)
 rows((; diagram)::PartitionOf) = mappedarray(r -> mappedarray(Returns(true), r), rows(diagram))
 
-shape(p::AbstractPartition) = p
+"""
+    shape(d::AbstractDiagram)
+
+Returns an `AbstractPartition` describing the shape of a diagram
+"""
 shape(d::AbstractDiagram) = PartitionOf(d)
+shape(p::AbstractPartition) = p
 
 struct EachIndexOf{D <: AbstractDiagram} <: AbstractYoungTableau{CartesianIndex{2}}
     diagram::D

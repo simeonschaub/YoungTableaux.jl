55c1f36c56029b0c4062477356fabfd504953aec
Simeon David Schaub <simeon@schaub.rocks>
2023-11-27 07:06:36 +0100

    add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using YoungTableaux
-using Test
+using Test, TestItems
 
-@testset "YoungTableaux.jl" begin
-    # Write your tests here.
+
+@testitem "basic functionality" begin
+    π = [4, 6, 3, 8, 1, 2, 7, 5]
+    P, Q = YoungTableaux.rs_pair(π)
+    P′, Q′ = YoungTableau{Int64}([[1, 2, 5], [3, 6, 7], [4, 8]]), YoungTableau{Int64}([[1, 2, 4], [3, 6, 7], [5, 8]])
+
+    @test P == P′
+    @test hash(P) == hash(P′)
+
+    @test nrows(P) == 3
+    @test ncols(P) == 3
+    @test ncols(P, 3) == 2
+    @test collect(P) == [1, 2, 5, 3, 6, 7, 4, 8]
+
+    @test shape(P) == Partition([3, 3, 2])
+    @test hash(shape(P)) == hash(Partition([3, 3, 2]))
+
+    @test eachindex(P) == eachindex(shape(P′))
+
+    @test occursin("<table", repr("text/html", P))
 end

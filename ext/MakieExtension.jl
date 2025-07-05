8c844ac0b7c92dacf713570e81727a3f02fd39ab
Simeon David Schaub <simeon@schaub.rocks>
2025-07-05 15:10:07 +0200

    upgrade to Makie 0.24
    requires MakieOrg/Makie.jl#5154

--- ext/MakieExtension.jl
+++ ext/MakieExtension.jl
@@ -20,7 +20,7 @@ end
 
 @recipe(YoungTableauPlot, yt) do scene
     Attributes(
-        text_attributes = (;),
+        text_attributes = Attributes(),
         color = :transparent,
         strokewidth = 1,
     )
@@ -29,18 +29,15 @@ end
 Makie.plottype(::AbstractYoungTableau) = YoungTableauPlot
 
 function Makie.plot!(p::YoungTableauPlot)
-    yt = p[:yt]
-    poly!(p, Makie.shared_attributes(p, Poly), map(shape, yt))
-    text = map(yt) do yt
-        map(string, yt)
-    end
-    positions = map(yt) do yt
-        map(eachindex(yt)) do I
-            Point2f(I[2] - 0.5, I[1] - 0.5)
+    map!(p.attributes, [:yt], [:shape, :text, :positions]) do yt
+        positions = map(eachindex(yt)) do I
+            Point2f(I[2] - 0.5f0, I[1] - 0.5f0)
         end
+        return shape(yt), map(string, yt), positions
     end
-    text_attributes = merge(Attributes(; align = (:center, :center)), p[:text_attributes])
-    text!(p, text_attributes, positions; text)
+    poly!(p, Makie.shared_attributes(p, Poly), p.shape)
+    text_attributes = merge(Attributes(; align = (:center, :center)), p.text_attributes[])
+    text!(p, text_attributes, p.positions; p.text)
     return p
 end
 
